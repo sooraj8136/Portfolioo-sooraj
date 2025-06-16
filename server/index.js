8d8fc6c9bef216e5f1a104c868f9b49b9c4c31e5
Soorajcp <168804842+Soorajcp@users.noreply.github.com>
2025-06-16 22:04:47 +0530

    index.js changed

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const app = express()
 connectDB()
 
 app.use(cors({
-    origin: ['http://localhost:5173','https://portfolioo-sooraj.vercel.app'],
+    origin: ['http://localhost:5173','https://portfolioo-client.vercel.app'],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }));
